f0886eb8cd7d1e9af2feefc7676a7c6c97777142
민종 <109796814+Adamise@users.noreply.github.com>
2022-11-22 19:27:13 +0900

    Add files via upload

---
+++ add.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#modalY").click(function () {
+        newEvent(first,second,type);
+    });
+});
---
+++ testing.js
@@ -0,0 +1,7 @@
+// var time = $("#member_table:eq(4) td").text();
+
+        var time = '2022-08-08 ~ 2022-12-31'
+        var changetoArr = time.split(' ');
+        console.log(Array.isArray(changetoArr));
+        console.log(changetoArr);
+        console.log(changetoArr[2]);
